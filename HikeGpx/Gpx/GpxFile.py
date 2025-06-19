53fdac8c7f4ba8d225dbbd6918eb084674930e68
saemi <saemi.stauber@gmx.ch>
2025-06-19 05:22:37 +0200

    some work from e14

--- HikeGpx/Gpx/GpxFile.py
+++ HikeGpx/Gpx/GpxFile.py
@@ -5,11 +5,17 @@ from .Waypoint import Waypoint
 
 class GpxFile:
     def __init__(self, name):
-        self.root = bs4.BeautifulSoup(features='xml')
-        self.gpx_tag = self.root.new_tag("gpx", attrs={"version": "1.0"})
-        self.root.append(self.gpx_tag)
-        self.name_tag = self.root.new_tag("name")
-        self.gpx_tag.append(self.name_tag)
+        self.bs = bs4.BeautifulSoup()
+        self.root = self.bs.new_tag("gpx", attrs={
+            "version": "1.1",
+            "xmlns": "http://www.topografix.com/GPX/1/1",
+            "creator": "HikeGpx Python Module by Samuel Stauber",
+        })
+        self.bs.append(self.root)
+        self.metadata_tag = self.bs.new_tag("metadata")
+        self.name_tag = self.bs.new_tag("name")
+        self.root.append(self.metadata_tag)
+        self.metadata_tag.append(self.name_tag)
         self.set_name(name)
         self.tracks: list[Track] = []
         self.waypoints: list[Waypoint] = []
@@ -27,13 +33,13 @@ class GpxFile:
         if track.number in existing_numbers:
             raise ValueError(f"Track number '{track.number}' already exists")
         self.tracks.append(track)
-        self.gpx_tag.append(track.get_soup(self.root))
+        self.root.append(track.get_soup(self.bs))
 
     def add_waypoint(self, waypoint: Waypoint):
-        self.gpx_tag.append(waypoint.get_soup(self.root))
+        self.root.append(waypoint.get_soup(self.bs))
         self.waypoints.append(waypoint)
 
     def save(self, path):
         with open(path, "w", encoding="utf8") as f:
-            f.write(self.root.prettify())
+            f.write(self.bs.prettify())
     
--- HikeGpx/geoadmin.py
+++ HikeGpx/geoadmin.py
@@ -8,14 +8,15 @@ def get_height_from_coordinate(coord: Tuple[LV95, WGS84]) -> float:
     if isinstance(coord, WGS84):
         coord = lv95_from_wgs84(coord)
     # coord is of type LV95
-    URI = "https://api3.geo.admin.ch/rest/services/height"
-    QUERY = f"easting={coord.E}&northing={coord.N}"
+    BASE_URL = "https://api3.geo.admin.ch/rest/services/height"
+    QUERY = f"?easting={coord.E}&northing={coord.N}"
+    URI = BASE_URL + QUERY
     try:
-        resp = requests.get(URI + "?" + QUERY).json()
+        resp = requests.get(URI).json()
     except Exception as e:
         raise Exception(f"Unable to perform GET-Request for altitude retrieval ({repr(e)}).")
     try:
         height = float(resp["height"])
     except Exception as e:
-        raise Exception(f"Unable to obtain altitude from GET-Response ({URI + "?" + QUERY}): {resp} ({repr(e)}).")
+        raise Exception(f"Unable to obtain altitude from GET-Response ({URI}): {resp} ({repr(e)}).")
     return height
